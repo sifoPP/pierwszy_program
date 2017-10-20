07fdd8f7966a4b92b5c9be6223880eba16f1dd14
wspolpp <wspolPP>
2017-10-20 09:39:47 +0200

    stworzylem_pierwszy_program

--- program1.c
+++ program1.c
@@ -6,7 +6,16 @@ int main() /* fukcja glowna program */
 /* int -> typ zwracanej wartosci (int - wartosc calkowita */
 { 
           puts("Pierwszy program dziala"); /* puts - funkcja z biblioteki standardowej */
+                                    /* puts -> put starting */
                                     /* wyswietlajaca ciag znakow */
+
+          putchar('g');
+          putchar('\n');   /* znak konca linii*/
+          putchar('\t');   /* tabulator*/ 
+          putchar('\r');   /*powrot karetki - kursor wraca na poczatek lini*/
+          putchar('\\');   /*wypisuje \ */
+
+       
            return 0;
-}
+}/* klamry oznaczaja blok programu */
 
