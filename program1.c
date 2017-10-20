b6ff89c1f29ac25e65e44af80bad5fe2e0844dc3
wspolpp <wspolPP>
2017-10-20 09:15:57 +0200

    stworzylem_pierwszy_program

---
+++ program1.c
@@ -0,0 +1,12 @@
+#include <stdio.h> /* biblioteka standardowa input-output */
+/* y = f(x) */
+/*     f() => 0 */
+
+int main() /* fukcja glowna program */
+/* int -> typ zwracanej wartosci (int - wartosc calkowita */
+{ 
+          puts("Pierwszy program dziala"); /* puts - funkcja z biblioteki standardowej */
+                                    /* wyswietlajaca ciag znakow */
+           return 0;
+}
+
